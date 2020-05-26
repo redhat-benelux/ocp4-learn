5b5196e69c7b29b0b0ecc79935df6843d19402a7
Raif Ahmed <rahmed@redhat.com>
2020-05-26 11:39:56 +0200

    clean code to delete everything, object storage creation

--- ibm-cloud/create_cluster.sh
+++ ibm-cloud/create_cluster.sh
@@ -1,11 +1,41 @@
 export DataCenterZone=ams03
-export ClusterName="my_openshift"
+export ClusterName="my-openshift"
 
-ibmcloud login -a https://api.eu-de.bluemix.net -r eu-de -u rahmed@redhat.com -p Welcome007 -c 63cf37b8c3bb448cbf9b7507cc8ca57d -g benelux
+# Ensure cluster name is lower case and no _,as it makes some issues to s3
+sed -e 's/\(.*\)/\L\1/' <<< "$ClusterName"
+
+
+
+# ibmcloud login -a https://api.eu-de.bluemix.net -r eu-de -u rahmed@redhat.com -p xxxxx -c 63cf37b8c3bb448cbf9b7507cc8ca57d -g benelux
+
+export PrivateVlanId=$(ibmcloud sl vlan list -d $DataCenterZone --output json | jq '.[] | select(.networkSpace=="PRIVATE")' | jq ."id")
+
+echo "private is ${PrivateVlanId}"
+if [ "${PrivateVlanId}" == "" ]; then
+  # Create VPC for deployment
+  echo "Createing Private VPC"
+  ibmcloud sl vlan create -t private -d $DataCenterZone -f
+fi
+
+export PrivateVlanId=$(ibmcloud sl vlan list -d $DataCenterZone --output json | jq '.[] | select(.networkSpace=="PRIVATE")' | jq ."id")
+
+
+export PublicVlanId=$(ibmcloud sl vlan list -d $DataCenterZone --output json | jq '.[] | select(.networkSpace=="PUBLIC")' | jq ."id")
+ 
+echo "public is ${PublicVlanId}"
+if [ "${PublicVlanId}" == "" ]; then
+  # Create VPC for deployment
+  echo "Createing Public VPC"
+  ibmcloud sl vlan create -t public -d $DataCenterZone -f
+fi
 
 export PublicVlanId=$(ibmcloud sl vlan list -d $DataCenterZone --output json | jq '.[] | select(.networkSpace=="PUBLIC")' | jq ."id")
 
-export PrivateVlanId=$(ibmcloud sl vlan list -d $DataCenterZone --output json | jq '.[] | select(.networkSpace=="PRIVATE")' | jq ."id")
+if [[ -z "${PrivateVlanId}" ]] || [[ -z "${PublicVlanId}" ]]; then
+  echo "can not create cluster as vlan creation failed" >&2
+  exit 1
+fi
+
 
 ibmcloud oc cluster create classic --name $ClusterName --location $DataCenterZone --version 4.3_openshift --flavor b3c.4x16.encrypted  --workers 3 --public-vlan $PublicVlanId --private-vlan $PrivateVlanId --public-service-endpoint
 
@@ -13,5 +43,42 @@ while [ "$(ibmcloud ks cluster ls --json | jq --arg ClusterName $ClusterName '.[
   echo "not yet deployed, sleeping..."
   sleep 90
 done
+
+
+# To Get the object storage service name
+# ibmcloud catalog service-marketplace | grep "storage"
+
+# To Get the object storage service plan
+# ibmcloud catalog service cloud-object-storage
+
+
+# Creating the IBM object storage service
+ibmcloud resource service-instance-create "$ClusterName"-cos cloud-object-storage standard global
+
+export COSServiceId=$(ibmcloud resource service-instance "$ClusterName"-cos --output json | jq '.[]'|  jq .'crn')
+
+sed -e 's/^"//' -e 's/"$//' <<<"$COSServiceId"
+
+
+echo "COSServiceId=$COSServiceId"
+# Creating credentials for IBM object storage service
+ibmcloud resource service-key-create "$ClusterName"_creds Writer --instance-name "$ClusterName"-cos
+
+export CredServiceKeyId=$(ibmcloud resource service-key "$ClusterName"-creds --output json | jq '.[]'|  jq .'crn')
+export CredAPIKey=$(ibmcloud resource service-key "$ClusterName"-creds  --output json | jq '.[]'|  jq .'credentials.apikey')
+
+echo "CredServiceKeyId=$CredServiceKeyId"
+echo "CredAPIKey=$CredAPIKey"
+
+
+export BucketName="$ClusterName"-bucket
+echo "BucketName=$BucketName"
+
+ibmcloud cos config auth --method IAM
+ibmcloud cos create-bucket --bucket "$BucketName" --ibm-service-instance-id $COSServiceId --region eu-de
+
+#ibmcloud ks kms
+
+#aws --endpoint-url=https://s3.ams.eu.cloud-object-storage.appdomain.cloud s3 ls
 
 exit;
