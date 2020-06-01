82269e06a72ab7b7bc102a6e81b9d87ea02577b1
Raif Ahmed <rahmed@redhat.com>
2020-06-01 22:34:53 +0200

    Initial Version

---
+++ sealed-secrets/00_build.sh
@@ -0,0 +1,8 @@
+export Version=v0.12.4
+export DownloadUrl="https://github.com/bitnami-labs/sealed-secrets/releases/download/${Version}/controller.yaml"
+
+curl -L ${DownloadUrl} --output ./base/controller.yaml
+
+oc apply -k base/
+
+exit;
