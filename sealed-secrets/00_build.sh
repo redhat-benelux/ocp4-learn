33e4c0592a8ff8a96abb0e3ad9ca1f791f5a5743
Raif Ahmed <rahmed@redhat.com>
2020-06-02 10:02:14 +0200

    Initial Version

--- sealed-secrets/00_build.sh
+++ sealed-secrets/00_build.sh
@@ -5,4 +5,7 @@ curl -L ${DownloadUrl} --output ./base/controller.yaml
 
 oc apply -k base/
 
+wget https://github.com/bitnami-labs/sealed-secrets/releases/download/${Version}/kubeseal-linux-amd64 -O kubeseal
+sudo install -m 755 kubeseal /usr/local/bin/kubeseal
+
 exit;
